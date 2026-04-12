62ee32a44f88dfe96f3dd07f7699f2729cc9316f
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-04-12 09:53:44 +0530

    Add files via upload

---
+++ itertools_product.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+A = []
+B = []
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+for i in product(A, B):
+    print(i, end=" ")
